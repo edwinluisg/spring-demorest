78cee646ecfdb8bfc3204f90cd50c31435d413fd
edwinluisg <luis.edwin06@gmail.com>
2023-10-21 18:30:11 -0400

    Update Entity Category

--- src/main/java/com/example/demorest/model/Category.java
+++ src/main/java/com/example/demorest/model/Category.java
@@ -3,7 +3,12 @@ package com.example.demorest.model;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.hibernate.id.CompositeNestedGeneratedValueGenerator.GenerationContextLocator;
 
 @Entity
 @Table(name="categories")
@@ -13,6 +18,8 @@ public class Category implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String description;
