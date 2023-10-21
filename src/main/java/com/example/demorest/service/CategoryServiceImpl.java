ab0a407793fd80d1184978da32bcfd0e51450f9a
edwinluisg <luis.edwin06@gmail.com>
2023-10-21 18:34:53 -0400

    Add services categories

---
+++ src/main/java/com/example/demorest/service/CategoryServiceImpl.java
@@ -0,0 +1,41 @@
+package com.example.demorest.service;
+
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demorest.model.Category;
+import com.example.demorest.model.dao.ICategoryDao;
+import com.example.demorest.response.CategoryResponseRest;
+
+@Service
+public class CategoryServiceImpl implements ICategoryService{
+	
+	private static final Logger log = LoggerFactory.getLogger(CategoryServiceImpl.class);
+	
+	@Autowired
+	private ICategoryDao categoryDao;
+	
+
+	@Transactional(readOnly = true)
+	public CategoryResponseRest searchCategories() {
+		log.info("init searchCategories");
+		CategoryResponseRest response = new CategoryResponseRest();
+		try {
+			List<Category> category = (List<Category>) categoryDao.findAll();
+			response.getCategoriesResponse().setCategories(category);
+			response.setMetadata("Response OK", "00", "Response success");
+		} catch (Exception e) {
+			response.setMetadata("Response NO", "-1", "Response error");
+			log.error("Error in searchCategories:", e.getMessage());
+			e.getStackTrace();
+		}
+		return response;
+	}
+
+}
---
+++ src/main/java/com/example/demorest/service/ICategoryService.java
@@ -0,0 +1,9 @@
+package com.example.demorest.service;
+
+import com.example.demorest.response.CategoryResponseRest;
+
+public interface ICategoryService {
+
+	public CategoryResponseRest searchCategories();
+
+}
