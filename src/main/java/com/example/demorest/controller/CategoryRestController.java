dc45fe528bef25f273baa2c87e68646baf6aad92
edwinluisg <luis.edwin06@gmail.com>
2023-10-21 18:36:03 -0400

    Add class category controller

---
+++ src/main/java/com/example/demorest/controller/CategoryRestController.java
@@ -0,0 +1,24 @@
+package com.example.demorest.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demorest.response.CategoryResponseRest;
+import com.example.demorest.service.ICategoryService;
+
+@RestController
+@RequestMapping("/v1")
+public class CategoryRestController {
+	
+	@Autowired
+	private ICategoryService service;
+	
+	@GetMapping("/categories")
+	public CategoryResponseRest consultaCategory() {
+		CategoryResponseRest response = service.searchCategories();
+		return response;
+	}
+
+}
